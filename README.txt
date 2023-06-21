b587a4ae6151440ac866c0d7746c36d55a2c56af
ThatT101Guy <67875344+SpartanT101@users.noreply.github.com>
2023-06-21 16:12:49 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+README
+Simple drag and drop. Default key is "L"
+
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+file 'transparent.css'
+
+chat_theme 'transparent_chat' {
+    styleSheet = 'transparent.css'
+}
+
+dependency 'chat'
+
+fx_version 'adamant'
+game 'common'
